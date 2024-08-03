1e16ce209b5ad27f91abbe2d3c9b368035a442d1
JJ-LazSoc <jack.jarrett@lazsoc.ca>
2024-08-03 18:57:33 -0400

    fixed negative days and miles

--- CarRentalGUI.java
+++ CarRentalGUI.java
@@ -182,6 +182,16 @@ public class CarRentalGUI extends JFrame {
                 int rentalDays = Integer.parseInt(rentalDaysTextField.getText());
                 int milesDriven = Integer.parseInt(milesDrivenTextField.getText());
 
+                // check if days and miles are greater than 0
+                if (rentalDays <= 0) {
+                    JOptionPane.showMessageDialog(CarRentalGUI.this, "Rental days must be greater than 0.");
+                    return;
+                }
+                if (milesDriven <= 0) {
+                    JOptionPane.showMessageDialog(CarRentalGUI.this, "Miles driven must be greater than 0.");
+                    return;
+                }
+
                 String customerName = customerNameTextField.getText();
                 String customerLicense = customerLicenseTextField.getText();
 
